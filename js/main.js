c32213a6f02c8463c2c10f38e339381e546ef956
SohelTanbir <soheltanbir19@gmail.com>
2022-06-30 11:36:21 +0600

    Finix web template

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    /*slider for logo*/
+    $('.logo_slider').slick({
+        autoplay:true,
+        slidesToShow:4,
+        speed:500,
+    });
+
+    /*sldier for Service section*/
+    $('.service_slider').slick({
+        autoplay:true,
+        slidesToShow:1,
+        speed:500,
+    });
+    /*sldier for Service section*/
+    $('.testimonial_main_content').slick({
+        autoplay:true,
+        slidesToShow:2,
+        speed:500,
+    });
+
+
+  });
